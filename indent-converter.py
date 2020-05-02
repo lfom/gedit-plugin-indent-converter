bcf3e78a008cbe86f6583f6d2826f7259570ad67
LFOM <lfom@users.noreply.github.com>
2020-05-02 18:37:09 -0300

    Menus moved to Tools sub-menu, added keyboard shortcuts

--- indent-converter.py
+++ indent-converter.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-from gi.repository import GObject, Gedit, Gtk, Gio
+import gi
+gi.require_version('Gedit', '3.0')
+from gi.repository import GObject, Gio, Gdk, Gedit
 import re
 try:
     import gettext
@@ -9,20 +11,45 @@ try:
 except:
     _ = lambda s: s
 
+#Code based on https://github.com/theawless/Clear-Doc
+class IndentConverterPluginAppActivatable(GObject.Object, Gedit.AppActivatable):
+    app = GObject.property(type=Gedit.App)
+    __gtype_name__ = "IndentConverterPluginAppActivatable"
 
-UI_XML = """<ui>
-    <menubar name='MenuBar'>
-        <menu name='EditMenu' action='Edit'>
-            <placeholder name='EditOps_6'>
-                <menu action='TabConvert'>
-                    <menuitem action='SpacesToTabs'/>
-                    <menuitem action='TabsToSpaces'/>
-                </menu>
-            </placeholder>
-        </menu>
-    </menubar>
-</ui>"""
+    def __init__(self):
+        GObject.Object.__init__(self)
+        self.menu_ext = None
+        self.menu_item = None
 
+    def do_activate(self):
+        self._build_menu()
+
+    def _build_menu(self):
+        # Get the extension from tools menu        
+        self.menu_ext = self.extend_menu("tools-section")
+        
+        # This is the submenu which is added to a menu item and then inserted in tools menu.        
+        sub_menu = Gio.Menu()
+        sub_menu_item_spaces = Gio.MenuItem.new(_("_Spaces to tabs"), 'win.spaces_to_tabs')
+        sub_menu.append_item(sub_menu_item_spaces)
+        sub_menu_item_tabs = Gio.MenuItem.new(_("_Tabs to Spaces"), 'win.tabs_to_spaces')
+        sub_menu.append_item(sub_menu_item_tabs)
+        self.menu_item = Gio.MenuItem.new_submenu(_("_Indent Converter"), sub_menu)
+        self.menu_ext.append_menu_item(self.menu_item)
+        
+        # Setting accelerators, first action is called when Ctrl+Alt+e is pressed. (PS: for some reason, using S and T do not work)
+        self.app.set_accels_for_action("win.spaces_to_tabs", ("<Primary><Alt>E", None))
+        self.app.set_accels_for_action("win.tabs_to_spaces", ("<Primary><Alt>A", None))
+
+    def do_deactivate(self):
+        self._remove_menu()
+
+    def _remove_menu(self):
+        # removing accelerators and destroying menu items
+        self.app.set_accels_for_action("win.spaces_to_tabs", ())
+        self.app.set_accels_for_action("win.tabs_to_spaces", ())
+        self.menu_ext = None
+        self.menu_item = None
 
 class IndentConverterPluginWindowActivatable(GObject.Object, Gedit.WindowActivatable):
 
@@ -34,23 +61,10 @@ class IndentConverterPluginWindowActivatable(GObject.Object, Gedit.WindowActivat
         GObject.Object.__init__(self)
 
     def do_activate(self):
-        self.insert_menu()
-        
-        #Add contextual menu entries
-        action = Gio.SimpleAction(name="spacestotabs")
-        action.connect('activate', lambda a, p: self.do_spaces_to_tabs())
-        self.window.add_action(action)
-
-        action = Gio.SimpleAction(name="tabstospaces")
-        action.connect('activate', lambda a, p: self.do_tabs_to_spaces())
-        self.window.add_action(action)
+        self._connect_menu()
 
     def do_deactivate(self):
-        self.remove_menu()
-        
-        #Remove contextual menu entries
-        self.window.remove_action("spacestotabs")
-        self.window.remove_action("tabstospaces")
+        pass
 
     def do_update_state(self):
         view = self.window.get_active_view()
@@ -58,81 +72,19 @@ class IndentConverterPluginWindowActivatable(GObject.Object, Gedit.WindowActivat
         #Enable if view exists and document is editable
         enabled = False
         enabled = view and view.get_editable()
-        self.window.lookup_action('spacestotabs').set_enabled(enabled)
-        self.window.lookup_action('tabstospaces').set_enabled(enabled)
-        
-        #self.action_group.set_sensitive(bool(view and view.get_editable()))
+        self.window.lookup_action('spaces_to_tabs').set_enabled(enabled)
+        self.window.lookup_action('tabs_to_spaces').set_enabled(enabled)
 
-    def insert_menu(self):
-        pass
-#        manager = self.window.get_ui_manager()
-#        self.action_group = Gtk.ActionGroup('IndentConverterActions')
-#        self.action_group.add_actions([
-#            ('TabConvert', None, _('Convert Tabs'), None, None, None),
-#            ('SpacesToTabs', Gtk.STOCK_INDENT, _('Convert spaces to tabs'),
-#                None, _('Convert spaces to tabs'), self.do_spaces_to_tabs),
-#            ('TabsToSpaces', Gtk.STOCK_INDENT, _('Convert tabs to spaces'),
-#                None, _('Convert tabs to spaces'), self.do_tabs_to_spaces),
-#        ])
-#        manager.insert_action_group(self.action_group, -1)
-#        self.ui_id = manager.add_ui_from_string(UI_XML)
+    def _connect_menu(self):
+        action_spaces = Gio.SimpleAction(name='spaces_to_tabs')
+        action_spaces.connect('activate', self.do_spaces_to_tabs)
+        self.window.add_action(action_spaces)
+        action_tabs = Gio.SimpleAction(name='tabs_to_spaces')
+        action_tabs.connect('activate', self.do_tabs_to_spaces)
+        self.window.add_action(action_tabs)
 
     def remove_menu(self):
         pass
-#        manager = self.window.get_ui_manager()
-#        manager.remove_ui(self.ui_id)
-#        manager.remove_action_group(self.action_group)
-#        manager.ensure_update()
-
-    def do_spaces_to_tabs(self):
-        view = self.window.get_active_view()
-        if view and hasattr(view, "indent_converter_plugin_view_activatable"):
-            view.indent_converter_plugin_view_activatable.do_spaces_to_tabs()
-    
-    def do_tabs_to_spaces(self):
-        view = self.window.get_active_view()
-        if view and hasattr(view, "indent_converter_plugin_view_activatable"):
-            view.indent_converter_plugin_view_activatable.do_tabs_to_spaces()
-            
-class IndentConverterPluginViewActivatable(GObject.Object, Gedit.ViewActivatable):   
-
-    __gtype_name__ = "IndentConverterPluginViewActivatable"
-
-    view = GObject.Property(type=Gedit.View)
-
-    def __init__(self):
-        self.popup_handler_id = 0
-        GObject.Object.__init__(self)
-
-    def do_activate(self):
-        self.view.indent_converter_plugin_view_activatable = self
-        self.popup_handler_id = self.view.connect('populate-popup', self.populate_popup)
-
-    def do_deactivate(self):
-        if self.popup_handler_id != 0:
-            self.view.disconnect(self.popup_handler_id)
-            self.popup_handler_id = 0
-        delattr(self.view, "indent_converter_plugin_view_activatable")
-
-    def populate_popup(self, view, popup):
-        if not isinstance(popup, Gtk.MenuShell):
-            return
-
-        item = Gtk.SeparatorMenuItem()
-        item.show()
-        popup.append(item)
-
-        item = Gtk.MenuItem.new_with_mnemonic(_("_Spaces to Tabs"))
-        item.set_sensitive(self.view.get_editable())
-        item.show()
-        item.connect('activate', lambda i: self.do_spaces_to_tabs())
-        popup.append(item)
-
-        item = Gtk.MenuItem.new_with_mnemonic(_('_Tabs to Spaces'))
-        item.set_sensitive(self.view.get_editable())
-        item.show()
-        item.connect('activate', lambda i: self.do_tabs_to_spaces())
-        popup.append(item)
 
     def tab_size(self):
         settings = Gio.Settings.new('org.gnome.gedit.preferences.editor')
@@ -168,9 +120,14 @@ class IndentConverterPluginViewActivatable(GObject.Object, Gedit.ViewActivatable
         else:
             return gcd(items[0], items[1])
 
-    def do_spaces_to_tabs(self):
+    def do_spaces_to_tabs(self, action, data):
+    
+        #TODO Code using view works?
+        #view = self.window.get_active_view()
+        #doc = view.get_buffer()
+    
         #Return if document is empty
-        doc = self.view.get_buffer()
+        doc = self.window.get_active_document()
         if doc is None:
             return
             
@@ -196,8 +153,8 @@ class IndentConverterPluginViewActivatable(GObject.Object, Gedit.ViewActivatable
         doc.set_text(text)
         doc.end_user_action()
 
-    def do_tabs_to_spaces(self):
-        doc = self.view.get_buffer()
+    def do_tabs_to_spaces(self, action, data):
+        doc = self.window.get_active_document()
         if doc is None:
             return
         
